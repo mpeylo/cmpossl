f0376f5d1627129f66747c7bc5c07faaf91df122
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2021-09-08 14:54:44 +0200

    cert_response(): Fix assignment of rkey for the central key generation case

--- crypto/cmp/cmp_client.c
+++ crypto/cmp/cmp_client.c
@@ -531,7 +531,7 @@ static int cert_response(OSSL_CMP_CTX *ctx, int sleep, int rid,
                          ossl_unused int req_type,
                          ossl_unused int expected_type)
 {
-    EVP_PKEY *rkey = OSSL_CMP_CTX_get0_newPkey(ctx /* may be NULL */, 0);
+    EVP_PKEY *rkey;
     int fail_info = 0; /* no failure */
     const char *txt = NULL;
     OSSL_CMP_CERTREPMESSAGE *crepmsg;
@@ -594,6 +594,7 @@ static int cert_response(OSSL_CMP_CTX *ctx, int sleep, int rid,
         return 0;
 
     subj = X509_NAME_oneline(X509_get_subject_name(cert), NULL, 0);
+    rkey = OSSL_CMP_CTX_get0_newPkey(ctx, 0);
     if (rkey != NULL
         /* X509_check_private_key() also works if rkey is just public key */
             && !(X509_check_private_key(ctx->newCert, rkey))) {
