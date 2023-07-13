42ec7a7f180515411fdf75aecb6a7d1f88a411e8
Rajeev Ranjan <ranjan.rajeev@siemens.com>
2023-07-13 16:25:56 +0200

    cmp_local.h; add ASN1 structure for OSSL_CMP_KEMCIPHERTEXTINFO

--- crypto/cmp/cmp_local.h
+++ crypto/cmp/cmp_local.h
@@ -242,6 +242,19 @@ struct ossl_cmp_crlstatus_st {
 }; /* OSSL_CMP_CRLSTATUS */
 DECLARE_ASN1_FUNCTIONS(OSSL_CMP_CRLSTATUS)
 
+/*
+ * KemCiphertextInfoValue :== KemCiphertextInfo
+ * KemCiphertextInfo ::= SEQUENCE {
+ *      kem       AlgorithmIdentifier{KEM-ALGORITHM, {...}},
+ *      ct        OCTET STRING}
+ */
+
+typedef struct ossl_cmp_KemCiphertextInfo_st {
+    X509_ALGOR *kem;
+    ASN1_OCTET_STRING *ct;
+} OSSL_CMP_KEMCIPHERTEXTINFO;
+DECLARE_ASN1_FUNCTIONS(OSSL_CMP_KEMCIPHERTEXTINFO)
+
 /*-
  * declared already here as it will be used in OSSL_CMP_MSG (nested) and
  * infoType and infoValue
@@ -303,6 +316,8 @@ struct ossl_cmp_itav_st {
         STACK_OF(OSSL_CMP_CRLSTATUS) *crlStatusList;
         /* NID_id_it_crls - Certificate Status Lists */
         STACK_OF(X509_CRL) *crls;
+        /* NID_id_it_KemCiphertextInfo -  KEM ciphertext */
+        OSSL_CMP_KEMCIPHERTEXTINFO *KemCiphertextInfoValue;
 
         /* this is to be used for so far undeclared objects */
         ASN1_TYPE *other;
