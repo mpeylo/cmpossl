0f11ab12e1d6500fef8dc8b9614b2b1361398492
Dr. David von Oheimb <dev@ddvo.net>
2025-04-03 08:53:58 +0200

    CMP: add OSSL_CMP_OPT_NONMATCHED_ERROR_NONCES accepting missing or non-matching transactionID or recipNonce in error message

--- crypto/cmp/cmp_local.h
+++ crypto/cmp/cmp_local.h
@@ -65,6 +65,7 @@ struct ossl_cmp_ctx_st {
      * certificate responses (ip/cp/kup), revocation responses (rp), and PKIConf
      */
     int unprotectedErrors;
+    int nonmatchedErrorNonces; /* accept missing/wrong transactionID or recipNonce values in error */
     int noCacheExtraCerts;
     X509 *srvCert; /* certificate used to identify the server */
     X509 *validatedSrvCert; /* caches any already validated server cert */
